4d9dc10b8da249152e6c9e8433a2d6d3a6b32365
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-27 23:39:00 -0500

    Working on one 1 = 100pts, two 1s = 200 pts

--- app/controllers/cubes_controller.rb
+++ app/controllers/cubes_controller.rb
@@ -1,9 +1,19 @@
 class CubesController < ApplicationController
   
   def keep
-    params[:cubes][:keep].each do |cube_id|
-      Cube.find(cube_id).keep
+    @player = Hand.first
+    if params[:cubes]
+        
+      #TODO make sure a submitted round of dice is scorable!!
+      @player.score
+      params[:cubes][:keep].each do |cube_id|
+        Cube.find(cube_id).keep
+      end
     end
-    redirect_to hand_path(1)
+    if @player.cubes.where(:held => false).count == 0
+      redirect_to "/new_roll"
+    else
+      redirect_to hand_path(1)
+    end
   end
 end
--- app/models/hand.rb
+++ app/models/hand.rb
@@ -3,6 +3,7 @@ class Hand < ActiveRecord::Base
   
   def new_roll
     self.round = 0
+    self.save
     Cube.all.order(:id).each do |cube|
       cube.face = nil
       cube.held = false
@@ -46,34 +47,57 @@ class Hand < ActiveRecord::Base
     self.straight(dice)
     self.three_of_kind(dice)
   end
+  
+  def save_score
+    self.total += self.round
+    self.save
+  end
 
   def straight(dice)
     if ([1, 2, 3, 4, 5, 6] - dice) == []
-      self.total += 1500
+      self.round += 1500
       self.save
     end
   end
   
   def three_of_kind(dice)
     by_number = dice.each_with_object(Hash.new(0)) { |face,counts| counts[face] += 1 }
-    for x in 2..5
+    for x in 2..6
       occurrences = by_number[x]
       if occurrences >= 3
-        self.total += x * 100
+        self.round += x * 100
         if occurrences >= 4
-          self.total += x * 100
+          self.round += x * 100
           if occurrences >= 5
-            self.total += x * 200
+            self.round += x * 200
             if occurrences >= 6
-              self.total += x * 400
+              self.round += x * 400
             end
           end
         end
       end
     end
-    self.save
   end
   
-  
-  
+  def ones(dice)
+    ones = by_number[1]
+    if ones == 1
+      self.round += 100
+    elsif ones == 2
+      self.round += 200
+    elsif ones >= 3
+      self.round += 1000
+      if ones >= 4
+        self.round += 1000
+        if ones >= 5
+          self.round += 2000
+          if ones >= 6 
+            self.round += 4000
+          end
+        end
+      end
+    end
+    binding.pry
+    self.save
+  end
 end
