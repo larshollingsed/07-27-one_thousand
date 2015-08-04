520a8f9d12928480b236eb813aac67aa5c860b60
Lars Hollingsed <larshollingsed@gmail.com>
2015-08-04 10:33:15 -0500

    Refactored scoring methods

--- app/models/hand.rb
+++ app/models/hand.rb
@@ -21,24 +21,6 @@ class Hand < ActiveRecord::Base
     end
   end
   
-  # Sorts dice into :held and :free Hashes then puts in an Array
-  # Returns an Array containing two Hashes of Cube objects
-  def show_dice
-    held_dice = {}
-    free_dice = {}
-    Cube.all.order(:id).each do |cube|
-      if cube.held
-        held_dice["die #{cube.id}"] = cube.face
-      else
-        free_dice["die #{cube.id}"] = cube.face
-      end
-    end
-    dice = {}
-    dice[:held] = held_dice
-    dice[:free] = free_dice
-    dice
-  end
-  
   # Gets the face values of all dice submitted
   # dice is an Array of dice_id's
   # Returns an Array of cube.face (Integers)
@@ -50,32 +32,19 @@ class Hand < ActiveRecord::Base
     values
   end
   
-  # Runs through the different ways to score
-  # TODO add self.save here instead of scoring methods!!
-  # dice - Array of dice_ids (!!!) to be scored
-  def score(dice)
-    dice_submitted = self.dice_values(dice)
-    if self.straight(dice_submitted)
-    elsif three_pairs(dice_submitted)
-    else
-      self.three_of_kind(dice_submitted)
-      self.ones(dice_submitted)
-      self.fives(dice_submitted)
-    end
-  end
-  
   # Checks to see if the submitted dice are capable of scoring
+  # dice is an Array of cube IDS!!!
   # Returns True if they can score, False if they can't
   def scorable?(dice)
-    starting_points = self.round
-    score(dice)
-    potential_score = self.round
-    if self.round > starting_points
-      self.round = starting_points
-      self.save
-      potential_score
-    end
+    score_dice(dice) > 0
   end
+  
+  # dice is an Array of cube IDS!!!! (not face values)
+  # returns score for those cubes
+  def score_dice(dice)
+    score(dice_values(dice))
+  end
+  
   
   # Saves round score to total score and resets round score
   # Returns self
@@ -84,13 +53,20 @@ class Hand < ActiveRecord::Base
     self.round = 0
     self.save
   end
-
-  # Scores 1500 if a 1-6 straight is submitted
-  # TODO take the 'dice' parameter out of all scoring methods
-  def straight(dice)
-    if ([1, 2, 3, 4, 5, 6] - dice) == []
-      self.round += 1500
-      self.save
+  
+  # gets the score for the submitted dice
+  # dice is an Array of face values
+  # returns the number of points earned
+  def score(dice)
+    points = 0
+    if straight?(dice)
+      1500
+    elsif three_pairs?(dice)
+      750
+    else
+      points += three_of_kind2(dice)
+      points += ones2(dice)
+      points += fives2(dice)
     end
   end
   
@@ -101,65 +77,76 @@ class Hand < ActiveRecord::Base
     dice.each_with_object(Hash.new(0)) { |face,counts| counts[face] += 1 }
   end
   
-  
-  # scores for three pairs (four of a kind != two pairs)
-  def three_pairs(dice)
-    if self.get_by_number(dice).values == [2, 2, 2]
-      self.round += 750
-    end
+  # checks for a six-dice straight
+  # dice is an Array of face values
+  # Returns True for a scoring roll or nil for a non-scoring roll
+  def straight?(dice)
+    ([1, 2, 3, 4, 5, 6] - dice) == []
   end
   
-  # scores for 3+ of a kind for 2, 3, 4, 5, and 6 (same scoring scale for multiples)
-  def three_of_kind(dice)
-    by_number = self.get_by_number(dice)
+  # checks for three pairs (four of a kind != two pairs)
+  # dice is an Array of face values
+  # returns True for a scoring roll or nil for a non scoring roll
+  def three_pairs?(dice)
+    get_by_number(dice).values == [2, 2, 2]
+  end
+  
+  def three_of_kind2(dice)
+    points = 0
+    by_number = get_by_number(dice)
     for x in [2, 3, 4, 5, 6]
       occurrences = by_number[x]
       if occurrences >= 3
-        self.round += x * 100
+        points += x * 100
         if occurrences >= 4
-          self.round += x * 100
+          points += x * 100
           if occurrences >= 5
-            self.round += x * 200
+            points += x * 200
             if occurrences >= 6
-              self.round += x * 400
+              points += x * 400
             end
           end
         end
       end
     end
+    points
   end
   
-  #scores for 1s
-  def ones(dice)
-    ones = self.get_by_number(dice)[1]
+  def ones2(dice)
+    points = 0
+    ones = get_by_number(dice)[1]
     if ones == 1
-      self.round += 100
+      points += 100
     elsif ones == 2
-      self.round += 200
+      points += 200
     elsif ones >= 3
-      self.round += 1000
+      points += 1000
       if ones >= 4
-        self.round += 1000
+        points += 1000
         if ones >= 5
-          self.round += 2000
+          points += 2000
           if ones >= 6 
-            self.round += 4000
+            points += 4000
           end
         end
       end
     end
-    self.save
+    points
   end
   
   # scores for one or two 5s
-  def fives(dice)
-    fives = self.get_by_number(dice)[5]
+  # dice is an Array of face values
+  # Returns number of points this is worth
+  def fives2(dice)
+    # this gets the number of fives
+    fives = get_by_number(dice)[5]
+    points = 0
     if fives == 1
-      self.round += 50
+      points = 50
     elsif fives == 2
-      self.round += 100
+      points += 100
     end
-    self.save
+    points
   end
   
 end
